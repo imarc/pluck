a502e7e78ab409d71301ffb0aa2c544129d71a31
Matthew J. Sahagian <msahagian@dotink.org>
2013-02-13 00:24:29 -0800

    Moved files around for copy & paste integration, added lab, added composer.

--- tests/bootstrap.php
+++
@@ -1,5 +0,0 @@
-<?php
-require __DIR__ . '/../src/Pluck/Document.php';
-require __DIR__ . '/../src/Pluck/Element.php';
-require __DIR__ . '/../src/Pluck/ElementList.php';
-require __DIR__ . '/../src/Pluck/Expression.php';
