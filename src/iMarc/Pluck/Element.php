fb38f01d4f4ef9a5837dbee0b69fe44e5dc05317
Matthew J. Sahagian <msahagian@dotink.org>
2013-02-14 14:00:03 -0800

    Added attr() to Element and recusive iterators/aggregators on ElementList
    The various aggregators and iterators are simple methods which take a
    callback and then "map" the results in a certain way.  For aggregate() it will
    merge all return values of the function into an array and return that.  For
    iteratorNew it will provide a new ElementList which you can work with in the
    callback and then return that ElementList itself (useful for filtered merging)
    and currently used on recursive find.

--- src/iMarc/Pluck/Element.php
+++ src/iMarc/Pluck/Element.php
@@ -16,7 +16,38 @@ namespace iMarc\Pluck;
  *
  * @todo write docs
  */
-class Element extends \DOMElement {
+class Element extends \DOMElement
+{
+	/**
+	 * Get or set an attribute on the element
+	 *
+	 * @access public
+	 * @param string $name The name of the attribute
+	 * @param string $name The name of the attribute
+	 * @param mixed $value The value to assign to the attribute (will be casted as a string)
+	 * @return mixed The string value of the attribute or the ELement if setting
+	 */
+	public function attr($name, $value = NULL)
+	{
+		if (func_num_args() == 1) {
+			return $this->hasAttribute($name)
+				? $this->getAttribute($name)
+				: NULL;
+		}
+
+		$this->setAttribute($name, $value);
+
+		return $this;
+	}
+
+
+	/**
+	 * Find children of this element matching a CSS selector
+	 *
+	 * @access public
+	 * @param string $selector A CSS selector for matching elements
+	 * @return ElementList An element list containing the elements matching the selector
+	 */
 	public function find($selector)
 	{
 		return $this->ownerDocument->find(
@@ -24,14 +55,30 @@ class Element extends \DOMElement {
 		);
 	}
 
+
+	/**
+	 * Get the text content value of this element
+	 *
+	 * @access public
+	 * @return string The inner text of the element
+	 */
 	public function text()
 	{
 		return $this->textContent;
 	}
 
+
+	/**
+	 * Remove the element from the DOM
+	 *
+	 * @access public
+	 * @return Element The element for method chaining
+	 */
 	public function remove()
 	{
 		$this->parentNode->removeChild($this);
+
+		return $this;
 	}
 }
 
--- src/iMarc/Pluck/ElementList.php
+++ src/iMarc/Pluck/ElementList.php
@@ -11,35 +11,207 @@
  */
 namespace iMarc\Pluck;
 
+use DOMNodeList;
+use Closure;
+
 /**
  * @author Jeff Turcotte <jeff@imarc.net>
+ * @author Matthew J. Sahagian [mjs] <matt@imarc.net>
  *
  * @todo write docs
  */
-class ElementList extends \ArrayIterator {
-	public function __construct(\DOMNodeList $list)
+class ElementList extends \ArrayIterator
+{
+	/**
+	 * Creates a new Element List
+	 *
+	 * @access public
+	 * @param DOMNodeList $list The DOMNodeList to mimic
+	 * @return void
+	 */
+	public function __construct(DOMNodeList $list = NULL)
 	{
-		$elements = array();
-		foreach($list as $element) {
-			array_push($elements, $element);
+		if ($list === NULL) {
+			$list = array();
 		}
-		parent::__construct($elements);
+
+		parent::__construct(array());
+
+		foreach($list as $element) {
+			$this[] = $element;
+		}
 	}
 
+
+	/**
+	 * Gets or sets an attribute
+	 *
+	 * @access public
+	 * @param string $name The name of the attribute
+	 * @param mixed $value The value to assign to the attribute (will be casted as a string)
+	 * @return Traversable All attribute values as an array if getting, this object if setting
+	 */
+	public function attr($name, $value = NULL)
+	{
+		if (func_num_args() == 1) {
+			return $this->aggregate(function($item) use ($name) {
+				return $item->attr($name);
+			});
+		}
+
+		$this->each(function($i, $item) use ($name, $value){
+			$item->attr($name, (string) $value);
+		});
+
+		return $this;
+	}
+
+
+	/**
+	 * Iterate over the internal list of elements
+	 *
+	 * @access public
+	 * @param string $map
+	 * @return ElementList The object for method chaining
+	 */
+	public function each(Closure $map)
+	{
+		$values = array();
+
+		foreach ($this as $i => $item) {
+			$values[] = $map($i, $item);
+		}
+
+		return $values;
+	}
+
+
+	/**
+	 * Merges the elements of another ElementList onto this one
+	 *
+	 * @access public
+	 * @param ElementList $list The ElementList to merge
+	 * @return ElementList The object for method chaining
+	 */
+	public function merge(ElementList $list)
+	{
+		if ($list != $this) {
+			foreach ($list as $item) {
+				$this[] = $item;
+			}
+		}
+
+		return $this;
+	}
+
+
+	/**
+	 * Find selector matching children of all elements in this list
+	 *
+	 * @access public
+	 * @param string $selector The selector to match against
+	 * @return ElementList A new ElementList containing matching children of this list's elements
+	 */
+	public function find($selector)
+	{
+		return $this->iterateNew(function($list, $item) use ($selector) {
+			return $list->merge($item->find($selector));
+		});
+	}
+
+
+	/**
+	 * Gets the element in the list at a provided index
+	 *
+	 * @access public
+	 * @param int $index The index position with which to get an element from
+	 * @return Element The Element if it exists, NULL otherwise
+	 */
 	public function get($index)
 	{
 		if (isset($this[$index])) {
 			return $this[$index];
 		}
+
+		return NULL;
 	}
 
-	public function __call($method, $args=array())
+
+	/**
+	 * Get the inner text of all elements in the list
+	 *
+	 * @access public
+	 * @return array An array containing the returned inner text for each element
+	 */
+	public function text()
 	{
-		foreach($this as $element) {
-			call_user_func_array(
-				array($element, $method), $args
-			);
-		}
+		return $this->aggregate(function($item) {
+			return $item->text();
+		});
 	}
+
+
+	/**
+	 * Remove all elements in the list from the DOM
+	 *
+	 * @access public
+	 * @return ElementList The object for method chaining
+	 */
+	public function remove()
+	{
+		$this->each(function($i, $item) {
+			unset($this[$i]);
+			return $item->remove();
+		});
+
+		return $this;
+	}
+
+
+	/**
+	 * Agregates the return values of a map into an array
+	 *
+	 * @access private
+	 * @param Closure $map The closure we map to
+	 * @return array The aggregate array
+	 */
+	private function aggregate(Closure $map)
+	{
+		$values = array();
+
+		foreach ($this as $item) {
+			$value = $map($item);
+
+			if (is_array($value)) {
+				$values = array_merge($values, $value);
+			} else {
+				$values[] = $value;
+			}
+		}
+
+		return $values;
+	}
+
+
+	/**
+	 * Iterates over the existing elements of a list using a map function
+	 *
+	 * @access private
+	 * @param Closure $map The closure we map to
+	 * @return ElementList A new element list potentially modfiied by the map
+	 */
+	private function iterateNew(Closure $map)
+	{
+		$list = new self();
+
+		foreach ($this as $item) {
+			$map($list, $item);
+		}
+
+		return $list;
+	}
+
+
+
 }
 
